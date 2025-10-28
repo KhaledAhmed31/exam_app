ebca8ef250ed64ebedd7533a30e7215a0cdf4648
khaled Ahmed <khaledahm79@gmail.com>
2025-10-28 15:35:18 +0300

    done: app colors

---
+++ lib/core/ui_manager/colors/app_colors.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color success = Color(0xFF11CE19);
+  static const Color error = Color(0xFFCC1010);
+  static const Color gray = Color(0xFF535353);
+  static const Color white = Color(0xFFF9F9F9);
+  static const Color lightBlue = Color(0xFFEDEFF3);
+  static const Color lightGreen = Color(0xFFCAF9CC);
+  static const Color lightRed = Color(0xFFF8D2D2);
+  static const Color darkBlue = Color(0xFF2C3A59);
+  static const Color darkGreen = Color(0xFF1E5128);
+  static const Color placholder = Color(0xFFA6A6A6);
+
+  //Black Shades
+  static const Color black10 = Color(0xFFCFCFCF);
+  static const Color black20 = Color(0xFFAFAFAF);
+  static const Color black30 = Color(0xFF878787);
+  static const Color black40 = Color(0xFF5F5F5F);
+  static const Color black50 = Color(0xFF373737);
+  static const Color blackBase = Color(0xFF0F0F0F);
+  static const Color black60 = Color(0xFF0D0D0D);
+  static const Color black70 = Color(0xFF0A0A0A);
+  static const Color black80 = Color(0xFF080808);
+  static const Color black90 = Color(0xFF050505);
+  static const Color black100 = Color(0xFF030303);
+
+  //Blue Shades
+  static const Color blue10 = Color(0xFFCCD7EB);
+  static const Color blue20 = Color(0xFFABBCDE);
+  static const Color blue30 = Color(0xFF809ACD);
+  static const Color blue40 = Color(0xFF5679BD);
+  static const Color blue50 = Color(0xFF2C58AC);
+  static const Color blueBase = Color(0xFF02369C);
+  static const Color blue60 = Color(0xFF022D82);
+  static const Color blue70 = Color(0xFF012468);
+  static const Color blue80 = Color(0xFF011B4E);
+  static const Color blue90 = Color(0xFF011234);
+  static const Color blue100 = Color(0xFF000B1F);
+}
